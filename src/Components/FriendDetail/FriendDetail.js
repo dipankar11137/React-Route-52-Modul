be2f193cc45f886bbffc0e40e82381c4b295f938
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-30 00:51:06 +0600

    show details

--- src/Components/FriendDetail/FriendDetail.js
+++ src/Components/FriendDetail/FriendDetail.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
     const { friendId } = useParams();
+    const [friend, setFriend] = useState({});
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${friendId}`)
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    }, [])
     return (
         <div>
             <h1>This is details of a Dosto : {friendId}</h1>
+            <h2>Name : {friend.name}</h2>
+            <p>Email : {friend.email}</p>
+            <p>City : {friend.address?.city}</p>
+            <p><small>Lat : {friend.address.geo.lat}</small></p>
+
         </div>
     );
 };
